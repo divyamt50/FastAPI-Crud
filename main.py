b81f3d203fb24158d957c73a93e9d69e42c882c8
divyamt50 <divyamt50@gmail.com>
2026-05-25 16:45:46 +0530

    added db queries for user create and user search by user id API

--- main.py
+++ main.py
@@ -14,43 +14,6 @@ Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
-posts:list[dict] = [
-    {
-        "id":1,
-        "title":"First blog",
-        "description":"this is my first blog",
-        "author":"Divyam",
-        "date_posted":"27 March 2027"
-    },
-    {
-        "id":2,
-        "title":"Second blog",
-        "description":"this is my second blog",
-        "author":"Divyam",
-        "date_posted":"27 March 2027"
-    },
-    {
-        "id":3,
-        "title":"Third blog",
-        "description":"this is my third blog",
-        "author":"Divyam",
-        "date_posted":"27 March 2027"
-    },
-    {
-        "id":4,
-        "title":"Fourth blog",
-        "description":"this is my fourth blog",
-        "author":"Divyam",
-        "date_posted":"27 March 2027"
-    },
-    {
-        "id":5,
-        "title":"Fifth blog",
-        "description":"this is my fifth blog",
-        "author":"Divyam",
-        "date_posted":"27 March 2027"
-    }
-]
 
 @app.get('/')
 def get_message():
@@ -70,24 +33,72 @@ def get_one_post(post_id:int):
         detail="post not found"
     )
 
-@app.post('/api/post', response_model=PostResponse)
-def add_post(post:PostCreate):
-    new_post = {
-            "id":len(posts) + 1,
-            "title":post.title,
-            "description":post.description,
-            "author":post.author,
-            "date_posted":"27 March 2027"
-        }
-    posts.append(new_post)
-    return new_post
+# @app.post('/api/post', response_model=PostResponse, status_code=status.HTTP_201_CREATED)
+# def add_post(post:PostCreate, db:Annotated[Session, Depends(get_db)]):
+#              result = 
+#     new_post = {
+#             "id":len(posts) + 1,
+#             "title":post.title,
+#             "description":post.description,
+#             "author":post.author,
+#             "date_posted":"27 March 2027"
+#         }
+#     posts.append(new_post)
+#     return new_post
 
-@app.exception_handler(RequestValidationError)
-def validation_exception_handler(request: Request, exception: RequestValidationError):
-    return JSONResponse(
-        status_code = status.HTTP_422_UNPROCESSABLE_CONTENT,
-        content = {"detail":exception.errors()}
+
+@app.post("/api/user", response_model=UserResponse, status = status.HTTP_201_CREATED)
+def create_user(user:UserCreate, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.User).where(models.User.username == user.username)
     )
+
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists"
+        )
+    
+    result = db.execute(
+        select(models.User).where(models.User.email == user.email)
+    )
+
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="email already exists"
+        )
+    
+    new_user = models.User(
+        username = user.username,
+        email = user.email
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/api/users/{user_id}', response_model=UserResponse)
+def get_user(user_id:int, db:Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.User).where(models.User.id == user_id)
+    )
+
+    user_instance = result.scalars().first()
+    
+    if user_instance:
+        return user_instance
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="User ID not found"
+    )
+
+
 
 @app.exception_handler(RequestValidationError)
 def validation_exception_handler(request:Request, exception:RequestValidationError):
