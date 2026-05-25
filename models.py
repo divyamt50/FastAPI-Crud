e1dbd714e73117e763fd782cb08c1afab8710c33
divyamt50 <divyamt50@gmail.com>
2026-05-25 14:41:07 +0530

    added sqlite models

---
+++ models.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from datetime import UTC, datetime
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id:Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username:Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email:Mapped[str] = mapped_column(String(120), unique = True, nullable = False)
+    image_file: Mapped[None|str] = mapped_column(String(200), nullable=True, default=None)
+
+    posts: Mapped[list[Post]] = relationship(back_populates="author")
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id:Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, index=True)
+    title:Mapped[str] = mapped_column(String(100), nullable=False)
+    description:Mapped[str] = mapped_column(Text, nullable=False)
+    user_id:Mapped[int] = mapped_column(ForeignKey=True, nullable=False, index = True)
+    date_posted:Mapped[datetime] = mapped_column(DateTime(timezone=True), default = lambda:datetime.now(UTC))
+
+    author:Mapped[User] = relationship(back_populates="posts")
